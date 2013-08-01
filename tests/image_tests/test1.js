08746477bd9ddc2d11fc5674c926f7187db57454
Justin Hornosty <justin@credil.org>
2013-08-01 17:44:53 -0400

    player example stuff

--- tests/image_tests/test1.js
+++ tests/image_tests/test1.js
@@ -5,6 +5,8 @@ var Game = {
     init: function() {}
 }
 
+
+
 var Game = {
     display: null,
  
@@ -15,28 +17,32 @@ var Game = {
 	Game._drawWholeMap();
     }
 }
-
-
+var freeCells =[];
 Game.map = {};
+
 Game._generateMap = function() {
     var digger = new ROT.Map.Digger();
-    var freeCells = [];
+
 
     var digCallback = function(x, y, value) {
 	var key = x+","+y;
         if (value) { 
-	    console.log(value);
-	    this.map[key] = "gfx/brick_gray1.png";//"o";
+	    // console.log(value);
+	    // this.map[key] = "gfx/brick_gray1.png";//"o";
 	    return; 
 	} /* do not store walls */
 	else{
-            freeCells.push(key)
+	    console.log("else");
+            freeCells.push(key);
             this.map[key] = "gfx/lair1.png";//"o";
 	}
     }
     digger.create(digCallback.bind(this));
-    this._generateBoxes(freeCells);
+//    this._generateBoxes(freeCells);
+    this._createPlayer(freeCells);
+
     this._drawWholeMap();
+
 }
 
 
@@ -53,6 +59,7 @@ Game._drawWholeMap = function() {
 	//this.display.draw(x, y, "x" );
 
     }
+    this.player._draw();
 }
 Game._generateBoxes = function(freeCells) {
     for (var i=0;i<10;i++) {
@@ -60,4 +67,26 @@ Game._generateBoxes = function(freeCells) {
         var key = freeCells.splice(index, 1)[0];
         this.map[key] = "gfx/unseen_item.png";
     }
+};
+
+
+var Player = function(x, y) {
+    this._x = x;
+    this._y = y;
+    this._draw();
+}
+ 
+Player.prototype._draw = function() {
+    Game.display.drawImage(this._x, this._y, "gfx/unseen_item.png");// "#ff0");
+}
+ 
+Game.player = null;
+ 
+Game._createPlayer = function(freeCells) {
+    var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+    var key = freeCells.splice(index, 1)[0];
+    var parts = key.split(",");
+    var x = parseInt(parts[0]);
+    var y = parseInt(parts[1]);
+    this.player = new Player(x, y);
 };

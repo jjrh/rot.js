244b2239daa7e3b99b8ce731045fd30f2b4e849b
Justin Hornosty <justin@credil.org>
2013-08-02 09:48:21 -0400

    player moving

--- tests/image_tests/test1.js
+++ tests/image_tests/test1.js
@@ -1,74 +1,99 @@
 // var immg = new Image;
 // ssrc="bomb.png"
 // immg.src = ssrc;
-var Game = {
-    init: function() {}
-}
+
 
 
 
 var Game = {
     display: null,
- 
+     map: {},
+    engine: null,
+    player: null,
+
     init: function() {
-        this.display = new ROT.Display();
-        document.body.appendChild(this.display.getContainer());
-	Game._generateMap();
-	Game._drawWholeMap();
-    }
-}
-var freeCells =[];
-Game.map = {};
+	this.display = new ROT.Display();
+	document.body.appendChild(this.display.getContainer());
+	this._generateMap();
 
-Game._generateMap = function() {
-    var digger = new ROT.Map.Digger();
+	var scheduler = new ROT.Scheduler.Simple();
+	scheduler.add(this.player, true);
+
+	this.engine = new ROT.Engine(scheduler);
+	this.engine.start();
 
 
-    var digCallback = function(x, y, value) {
-	var key = x+","+y;
-        if (value) { 
-	    // console.log(value);
-	    // this.map[key] = "gfx/brick_gray1.png";//"o";
-	    return; 
-	} /* do not store walls */
-	else{
-	    console.log("else");
-            freeCells.push(key);
-            this.map[key] = "gfx/lair1.png";//"o";
+	// Game._drawWholeMap();
+
+
+    },
+
+    _generateMap: function(){
+	var digger = new ROT.Map.Digger();
+	var freeCells = [];
+
+	var digCallback = function(x, y, value) {
+	    var key = x+","+y;
+            if (value) { 
+		// console.log(value);
+		this.map[key] = "gfx/brick_gray1.png";//"o";
+		return; 
+	    } /* do not store walls */
+	    else{
+		console.log("else");
+		freeCells.push(key);
+		this.map[key] = "gfx/lair1.png";//"o";
+	    }
 	}
-    }
-    digger.create(digCallback.bind(this));
-//    this._generateBoxes(freeCells);
-    this._createPlayer(freeCells);
+	digger.create(digCallback.bind(this));
+	//    this._generateBoxes(freeCells);
+	this._createPlayer(freeCells);
 
-    this._drawWholeMap();
+	this._drawWholeMap();
 
-}
-
-
-Game._drawWholeMap = function() {
-    console.log("done");
-    for (var key in this.map) {
-        var parts = key.split(",");
-        var x = parseInt(parts[0]);
-        var y = parseInt(parts[1]);
-	// this.display._spacingX = 64;
-	// this.display._spacingY = 64;
-        this.display.drawImage(x, y,this.map[key]);
 	
-	//this.display.draw(x, y, "x" );
 
-    }
-    this.player._draw();
+    },
+    
+    _generateBoxes: function(){
+	for (var i=0;i<10;i++) {
+            var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+            var key = freeCells.splice(index, 1)[0];
+            this.map[key] = "gfx/unseen_item.png";
+	}
+
+    },
+    
+    _drawWholeMap: function(){
+	console.log("done");
+	for (var key in this.map) {
+            var parts = key.split(",");
+            var x = parseInt(parts[0]);
+            var y = parseInt(parts[1]);
+	    // this.display._spacingX = 64;
+	    // this.display._spacingY = 64;
+            this.display.drawImage(x, y,this.map[key]);
+	    
+	    //this.display.draw(x, y, "x" );
+
+	}
+	this.player._draw();
+	
+    },
+
+    
+
+    _createPlayer: function(freeCells){
+	
+	var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+	var key = freeCells.splice(index, 1)[0];
+	var parts = key.split(",");
+	var x = parseInt(parts[0]);
+	var y = parseInt(parts[1]);
+	this.player = new Player(x, y);
+
+    },
 }
-Game._generateBoxes = function(freeCells) {
-    for (var i=0;i<10;i++) {
-        var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
-        var key = freeCells.splice(index, 1)[0];
-        this.map[key] = "gfx/unseen_item.png";
-    }
-};
-
 
 var Player = function(x, y) {
     this._x = x;
@@ -79,14 +104,120 @@ var Player = function(x, y) {
 Player.prototype._draw = function() {
     Game.display.drawImage(this._x, this._y, "gfx/unseen_item.png");// "#ff0");
 }
+
+Player.prototype.act = function() {
+    Game.engine.lock();
+    /* wait for user input; do stuff when user hits a key */
+    window.addEventListener("keydown", this);
+}
+
+Player.prototype.handleEvent = function(e) {
+    console.log(e);
+
+    var keyMap = {};
+    keyMap[38] = 0;
+    keyMap[33] = 1;
+    keyMap[39] = 2;
+    keyMap[34] = 3;
+    keyMap[40] = 4;
+    keyMap[35] = 5;
+    keyMap[37] = 6;
+    keyMap[36] = 7;
+    
+    var code = e.keyCode;
+    
+    if (!(code in keyMap)) { return; }
+    
+    var diff = ROT.DIRS[8][keyMap[code]];
+    var newX = this._x + diff[0];
+    var newY = this._y + diff[1];
+    
+    var newKey = newX + "," + newY;
+    if (!(newKey in Game.map)) { return; } /* cannot move in this direction */
+    /* process user input */
+
+    Game.display.draw(this._x, this._y, Game.map[this._x+","+this._y]);
+    this._x = newX;
+    this._y = newY;
+    this._draw();
+    window.removeEventListener("keydown", this);
+    Game.engine.unlock();
+
+}
  
-Game.player = null;
+
+
+Game.init();
+
+// var freeCells =[];
+// Game.map = {};
+
+// Game._generateMap = function() {
+//     var digger = new ROT.Map.Digger();
+
+
+//     var digCallback = function(x, y, value) {
+// 	var key = x+","+y;
+//         if (value) { 
+// 	    // console.log(value);
+// 	    // this.map[key] = "gfx/brick_gray1.png";//"o";
+// 	    return; 
+// 	} /* do not store walls */
+// 	else{
+// 	    console.log("else");
+//             freeCells.push(key);
+//             this.map[key] = "gfx/lair1.png";//"o";
+// 	}
+//     }
+//     digger.create(digCallback.bind(this));
+// //    this._generateBoxes(freeCells);
+//     this._createPlayer(freeCells);
+
+//     this._drawWholeMap();
+
+// }
+
+
+// Game._drawWholeMap = function() {
+//     console.log("done");
+//     for (var key in this.map) {
+//         var parts = key.split(",");
+//         var x = parseInt(parts[0]);
+//         var y = parseInt(parts[1]);
+// 	// this.display._spacingX = 64;
+// 	// this.display._spacingY = 64;
+//         this.display.drawImage(x, y,this.map[key]);
+	
+// 	//this.display.draw(x, y, "x" );
+
+//     }
+//     this.player._draw();
+// }
+// Game._generateBoxes = function(freeCells) {
+//     for (var i=0;i<10;i++) {
+//         var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+//         var key = freeCells.splice(index, 1)[0];
+//         this.map[key] = "gfx/unseen_item.png";
+//     }
+// };
+
+
  
-Game._createPlayer = function(freeCells) {
-    var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
-    var key = freeCells.splice(index, 1)[0];
-    var parts = key.split(",");
-    var x = parseInt(parts[0]);
-    var y = parseInt(parts[1]);
-    this.player = new Player(x, y);
-};
+// Game._createPlayer = function(freeCells) {
+//     var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+//     var key = freeCells.splice(index, 1)[0];
+//     var parts = key.split(",");
+//     var x = parseInt(parts[0]);
+//     var y = parseInt(parts[1]);
+//     this.player = new Player(x, y);
+// };
+
+
+
+// Game.engine = null;
+ 
+// // Game.init = function() {
+  
+
+// // }
+
